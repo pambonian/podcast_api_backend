10126b65dd7a1070047b8646717f538d3b878a2e
pambonian <pambonian@gmail.com>
2022-01-25 17:33:15 -0500

    bootstrap started

--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,22 @@ posts = [
         'author': 'CoreyMS',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'August 27, 2018'
+        'date_posted': 'August 27, 2018',
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
+        'date_posted': 'August 28, 2018',
+    },
 ]
 
 def home(request):
     context = {
-        'post': posts
+        'posts': posts
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
 

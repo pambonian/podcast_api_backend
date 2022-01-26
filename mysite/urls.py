a51fd057a68c2a3e936a5a19360829b720453658
onticinc <onticinc@gmail.com>
2022-01-25 20:20:41 -0700

    login finished

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from functools import lru_cache
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from users import views as user_views
 
@@ -24,6 +25,11 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('blog/', include('blog.urls',))
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
+
+    path('blog/', include('blog.urls',)),
 ]
 

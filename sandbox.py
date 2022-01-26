ea09ed30b7fb9e6c66ae022434999b89a7dc48a6
onticinc <onticinc@gmail.com>
2022-01-26 01:04:01 -0700

    created sandbox.py

---
+++ sandbox.py
@@ -0,0 +1,91 @@
+# 'id': 'tag:soundcloud,2010:tracks/325731840', 
+    # 'guidislink': False, 
+    # 'title': 'EGGS 001: Who are you and what the f*ck are you doing here?', 
+    # 'title_detail': 
+    #     {
+    #         'type': 'text/plain', 
+    #         'language': None, 
+    #         'base': 'https://feeds.simplecast.com/T8TzwY_T', 
+    #         'value': 'EGGS 001: Who are you and what the f*ck are you doing here?'
+    #     }, 
+    # 'summary': '<p>This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram</p>', 
+    # 'summary_detail': 
+    #     {
+    #         'type': 'text/html', 
+    #         'language': None, 
+    #         'base': 'https://feeds.simplecast.com/T8TzwY_T', 
+    #         'value': '<p>This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram</p>'
+    #     },
+    # 'published': 'Thu, 1 Jun 2017 00:00:00 +0000', 
+    # 'published_parsed': time.struct_time(tm_year=2017, tm_mon=6, tm_mday=1, tm_hour=0, tm_min=0, tm_sec=0, tm_wday=3, tm_yday=152, tm_isdst=0), 
+    # 'authors': 
+    #     [
+    #         {
+    #         'name': 'EGGS', 
+    #         'email': 'eggsthepodcast@gmail.com'
+    #         }, 
+    #         {
+    #         'name': 'EGGS'
+    #         }
+    #     ], 
+    # 'author': 'EGGS', 
+    # 'author_detail': 
+    #     {
+    #         'name': 'EGGS', 
+    #         'email': 'eggsthepodcast@gmail.com'
+    #     }, 
+    # 'links': 
+    #     [
+    #         {
+    #             'rel': 'alternate', 
+    #             'type': 'text/html', 
+    #             'href': 'http://www.eggscast.com'},
+    #         {
+    #             'length': '48389853', 
+    #             'type': 'audio/mpeg', 
+    #             'href': 'https://dts.podtrac.com/redirect.mp3/cdn.simplecast.com/audio/97afaf/97afaf96-662c-47e5-b011-e5d0bb3196b6/897ae2e5-3758-4d6f-a05f-7b1189beac0e/0e97b1a9_tc.mp3?aid=rss_feed&feed=T8TzwY_T', 
+    #             'rel': 'enclosure'
+    #         }
+    #     ],
+    # 'link': 'http://www.eggscast.com', 
+    # 'content': 
+    #     [
+    #         {
+    #             'type': 'text/html', 
+    #             'language': None, 
+    #             'base': 'https://feeds.simplecast.com/T8TzwY_T', 
+    #             'value': '<p>This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram</p>'
+    #         }, 
+    #         {   'type': 'text/plain', 
+    #             'language': None, 
+    #             'base': 'https://feeds.simplecast.com/T8TzwY_T', 
+    #             'value': 'This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram'    
+    #         }
+    #     ], 
+    # 'itunes_title': 'EGGS 001: Who are you and what the f*ck are you doing here?',
+    # 'image': 
+    #     {
+    #         'href': 'https://image.simplecastcdn.com/images/97afaf/97afaf96-662c-47e5-b011-e5d0bb3196b6/897ae2e5-3758-4d6f-a05f-7b1189beac0e/3000x3000/1529527124-artwork.jpg?aid=rss_feed'
+    #     }, 
+    # 'itunes_duration': '00:50:12', 
+    # 'subtitle': 'This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram', 
+    # 'subtitle_detail': 
+    #     {
+    #         'type': 'text/plain', 
+    #         'language': None, 
+    #         'base': 'https://feeds.simplecast.com/T8TzwY_T', 'value': 'This is it! The inaugural first episode of the EGGS podcast. EGGS is a podcast about all things creative, regardless of what that means to you. From the creativity involved in launching a business, building brand, crafting an entertainment career or whatever, we talk about the process of growing your EGG from start to finish. ---- The Plugs: Ryan R2 SLC/SPAIN r2mg.com @r2mg on twitter @r2mediagroup on instagram Mike DJ Ontic shows djontic.com @djontic on twitter The Show eggscast.com @eggshow on twitter and instagram'}, 'itunes_explicit': None, 'itunes_episodetype': 'full', 'itunes_episode': '1'}
+
+
+import feedparser
+
+url = 'https://feeds.simplecast.com/T8TzwY_T'
+
+def parse_rss_feed():
+    feed = feedparser.parse(url)
+
+    for entry in feed.entries:
+        print('****************************')
+        print(f"{entry}: {entry.link}")
+        print('****************************')
+    
+parse_rss_feed()

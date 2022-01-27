56f192b72608114ab487a3c97621844ff1a258c0
Michael Smith <onticinc@gmail.com>
2022-01-26 17:50:56 -0700

    Revert "Create update and delete"

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
 
+
+# Create your views here.
 
 def register(request):
     if request.method == 'POST':
@@ -10,26 +12,23 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(request, f'Your Account Has Been Created, You are now able to login. {username}!')
             return redirect('login')
     else:
-        form = UserRegisterForm()
+        form = UserRegisterForm
     return render(request, 'users/register.html', {'form': form})
-
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has been updated!')
+            messages.success(request, f'Your account has been updated')
             return redirect('profile')
-
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
@@ -40,3 +39,5 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
+
+   

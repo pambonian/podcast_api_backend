0508ffc66fb6d14e8d20f94296990f7e15070daf
pambonian <pambonian@gmail.com>
2022-01-28 17:20:30 -0500

    webscraper created

--- scraper.py
+++ scraper.py
@@ -4,135 +4,40 @@ import requests
 
 source = requests.get('https://feeds.simplecast.com/T8TzwY_T').text
 soup = BeautifulSoup(source, 'lxml')
-# print(soup.prettify())
-
-# for article in soup.find_all('div', class_='article'):
-#     headline = article.h2.a.text
-#     print(headline)
-
-# # pull a title
-# item = soup.find('item')
-# headline = item.title.text
-# # print(headline)
-
-# pull all titles
-# for item in soup.find_all('item'):
-    
-    # try:
-item = soup.find('item')
-
-headline = item.title.text
-
-description = soup.find('itunes:summary').text
-
-datepub = item.pubDate
-
-sitelink = item.link.text
-
-author = soup.find('itunes:author').text
-
-image_link = item.itunes
-
-item_results = [headline, description, datepub, sitelink, author, image_link]
-
-print(item_results)
 
 
-# print(sitelink)
-    # except Exception as e:
-    #     headline = None
-    #     description = None
-    #     datepub = None
-    #     author = None
-    #     sitelink = None
 
-# # pull embedded yt-video's source as html
-# vid_src = item.find('iframe', class_='youtube-player')
-# # print(vid_src)
-
-# # pull value of the source attribute from the tag
-# vid_src = item.find('iframe', class_='youtube-player')['src']
-
-# # parse the string to grab the value of the video (splits on all forward slash)
-# vid_id = vid_src.split('/')
-# # print(vid_id)
-
-# # specify index of split for everything after the fourth index of the / split
-# vid_id = vid_src.split('/')[4]
-# # print(vid_id)
-
-# # grab the video id from the split info returned
-# vid_id = vid_id.split('?')[0]
-
-# # create a new youtube link using the id you got from the parsing
-# yt_link = f'https://youtube.com/watch@v={vid_id}'
-# print(yt_link)
-
-#  LOOPING OVER EVERYTHING WITH WHAT YOU LEARNED ABOVE
-
-# # pull all titles
-# for item in soup.find_all('item'):
-#     headline = item.title.text
-    # print(headline)
-
-# try/accept for any missing loop items
-
-# for article in soup.find_all('article'):
-#     headline = article.h2.a.text
-#     # print(headline)
-
-#     summary = article.find('div', class_='entry-content').p.text
-#     # print(summary)
-
-#     try:
-#         vid_src = article.find('iframe', class_='youtube-player')['src']
-
-#         vid_id = vid_src.split('/')[4]
-#         vid_id = vid_id.split('?')[0]
-
-#         yt_link = f'https://youtube.com/watch?v={vid_id}'
-#     except Exception as e:
-#         yt_link = None
-
-    # print(yt_link)
+# for item in soup.findAll('item'):
+for item in soup.findAll('item'):
 
 
-# # TUT CODE
-# from bs4 import BeautifulSoup
-# import requests
-# import csv
+    site_title = item.find('title').text
+    itunes_episode = item.find('itunes:episode').text
+    itunes_title = item.find('itunes:title').text
+    itunes_author = item.find('itunes:author').text
+    itunes_duration = item.find('itunes:duration').text
+    itunes_explicit = item.find('itunes:explicit').text
+    itunes_summary = item.find('itunes:summary').text
+    cover_art = item.find('itunes:image')
+    media = item.find('enclosure')['url']
 
-# source = requests.get('http://coreyms.com').text
+    item_results = [
+        site_title, itunes_episode, itunes_title, itunes_author, itunes_duration, itunes_explicit, itunes_summary, cover_art, media
+    ]
 
-# soup = BeautifulSoup(source, 'lxml')
-# # open csv file:
-# csv_file = open('cms_scrape.csv', 'w')
-# # set up csv file:
-# csv_writer = csv.writer(csv_file)
-# write headers for csv file:
-# csv_writer.writerow(['headline', 'summary', 'video_link'])
-# find all articles:
-# for article in soup.find_all('article'):
-#     headline = article.h2.a.text
-    # print(headline)
-# find the text in p tags for divs with class entry content:
-    # summary = article.find('div', class_='entry-content').p.text
-    # print(summary)
-# ignore issues when looping when missing elements are detected using try/except:
-    # try:
-    #     vid_src = article.find('iframe', class_='youtube-player')['src']
+    print(item_results)
 
-    #     vid_id = vid_src.split('/')[4]
-    #     vid_id = vid_id.split('?')[0]
+try:
 
-    #     yt_link = f'https://youtube.com/watch?v={vid_id}'
-    # except Exception as e:
-    #     yt_link = None
+    print()
 
-    # print(yt_link)
-
-    # print()
-# write to csv:
-    # csv_writer.writerow([headline, summary, yt_link])
-# close csv:
-# csv_file.close()
+except Exception as e:
+        
+    site_title = None
+    itunes_episode = None
+    itunes_title = None
+    itunes_author = None
+    itunes_duration = None
+    itunes_explicit = None
+    cover_art = None
+    media = None

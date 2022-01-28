a94db02be9e681dad0512e3f057ac56ce198463a
pambonian <pambonian@gmail.com>
2022-01-28 14:33:43 -0500

    progress

--- scraper.py
+++ scraper.py
@@ -20,6 +20,31 @@ for item in soup.find_all('item'):
     headline = item.title.text
     # print(headline)
 
-# pull embedded yt-video's source
-vid_src = item.find('iframe', class_='youtube-player')['src']
+# pull embedded yt-video's source as html
+vid_src = item.find('iframe', class_='youtube-player')
 # print(vid_src)
+
+# pull value of the source attribute from the tag
+vid_src = item.find('iframe', class_='youtube-player')['src']
+
+# parse the string to grab the value of the video (splits on all forward slash)
+vid_id = vid_src.split('/')
+# print(vid_id)
+
+# specify index of split for everything after the fourth index of the / split
+vid_id = vid_src.split('/')[4]
+# print(vid_id)
+
+# grab the video id from the split info returned
+vid_id = vid_id.split('?')[0]
+
+# create a new youtube link using the id you got from the parsing
+yt_link = f'https://youtube.com/watch@v={vid_id}'
+# print(yt_link)
+
+#  LOOPING OVER EVERYTHING WITH WHAT YOU LEARNED ABOVE
+
+# pull all titles
+for item in soup.find_all('item'):
+    headline = item.title.text
+    # print(headline)

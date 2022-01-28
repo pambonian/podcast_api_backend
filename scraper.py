dc6c323d92c80da122648f2adcf20b98fdd92d63
pambonian <pambonian@gmail.com>
2022-01-28 14:23:06 -0500

    scraping progress

--- scraper.py
+++ scraper.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-with open('deleteme.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
 
-match = soup.find('div', class_='footer')
-print(match)
+source = requests.get('https://feeds.simplecast.com/T8TzwY_T').text
+soup = BeautifulSoup(source, 'lxml')
+# print(soup.prettify())
+
+# for article in soup.find_all('div', class_='article'):
+#     headline = article.h2.a.text
+#     print(headline)
+
+# pull a title
+item = soup.find('item')
+headline = item.title.text
+# print(headline)
+
+# pull all titles
+for item in soup.find_all('item'):
+    headline = item.title.text
+    # print(headline)
+
+# pull embedded yt-video's source
+vid_src = item.find('iframe', class_='youtube-player')['src']
+# print(vid_src)

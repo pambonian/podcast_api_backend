a36ec619aa58c457fee66d705363d667be136452
onticinc <onticinc@gmail.com>
2022-01-31 11:52:18 -0700

    debug heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
@@ -136,7 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-
+os.mkdir(os.path.join(BASE_DIR, 'static'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
